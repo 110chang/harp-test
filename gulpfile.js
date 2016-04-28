c9deabb0ddf4347f927005258873dd1a89d9e68c
110chang <u@110chang.com>
2016-04-28 19:50:02 +0900

    [add] static site sample
    - relative root path
    - meta data

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,42 @@
-var gulp = require('gulp');
+
 var harp = require('harp');
+var gulp = require('gulp');
+var yaml = require('gulp-yaml');
+var browsersync = require('browser-sync');
+var exec = require('child_process').exec;
 
 gulp.task('server', function() {
   harp.server(__dirname + '/public', {
     port: 3000
-  }, function() {
-    //
-  });
-})
+  }, function() {});
 
-gulp.task('default', ['server']);
+  browsersync.init({
+    proxy: 'http://localhost:3000/',
+    port: 3333,
+    open: false,
+    //logLevel: 'debug'
+  });
+});
+
+gulp.task('data', function() {
+  gulp.src('./public/**/*.yml')
+    .pipe(yaml({ schema: 'DEFAULT_SAFE_SCHEMA' }))
+    .pipe(gulp.dest('./public/'))
+    .pipe(browsersync.stream());
+});
+
+gulp.task('default', ['server'], function() {
+  gulp.watch('./public/**/*', function() {
+    browsersync.reload();
+  });
+  gulp.watch('./public/**/*.yml', ['data']);
+});
+
+gulp.task('compile', function() {
+  exec('harp compile ./public ./build', function(err, stdout, stderr) {
+    console.log(stdout);
+    console.log(stderr);
+    //cb(err);
+  });
+});
 
